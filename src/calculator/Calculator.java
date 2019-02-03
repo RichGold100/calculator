3cc109e9745c1d9dc494aad66afb586fd836993b
DESKTOP-4SCGLCJ\kwon <winwin47k@gmail.com>
2019-02-03 18:50:37 +0900

    2.값 입력 및 사칙연산 - 조건문

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -10,14 +10,32 @@ public class Calculator {
 		int first = scanner.nextInt();
 		System.out.println(first);		
 	
+		System.out.println("연산기호 입력(*/+-):");
+		String symbol = scanner.next();
+		System.out.println(symbol);
+		
+		
 		System.out.println("두번째 입력값:");
 		int second = scanner.nextInt();
 		System.out.println(second);
 		
-		System.out.println(first+"*"+second+"="+(first*second));
-		System.out.println(first+"/"+second+"="+(first/second));
-		System.out.println(first+"+"+second+"="+(first+second));
-		System.out.println(first+"-"+second+"="+(first-second));
+
+		
+		if("+".equals(symbol)){
+			System.out.println(first+"+"+second+"="+(first+second));
+		} else if ("-".equals(symbol)){
+			System.out.println(first+"-"+second+"="+(first-second));
+		} else if (symbol.equals("*")){
+			System.out.println(first+"*"+second+"="+(first*second));
+		} else if (symbol.equals("/")) {
+			System.out.println(first+"/"+second+"="+(first/second));
+		} else {
+			System.out.println("사칙연산해당되는 부호를 입력하지 않았습니다.");
+		}
+		
+		
+		
+		
 		
 		
 		
